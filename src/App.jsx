62a8e8d3fe66ac6659b5c820d45ab5f5aea80a40
Erhard Nagy <Erson1989x@gmail.com>
2024-05-24 21:25:11 +0300

    v01

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,58 @@
 import { useState } from 'react'
 import './App.css'
 import { resetButton } from './assets/components/functions.js';
+import { listOfColors } from './assets/components/listOfColors.js';
+
+
+
 
 function App() {
   // Use state
-  const [listOfColors, setListOfColors] = useState(['#AD636C', '#009B72', '#6761A8', '#347E8D', '#798186', '#B4D2BA', '#B57F50', '#8ED081', '#F00000', '#bdc3c7']);
-  const [currentColor, setCurrentColor] = useState('#AD636C');
   const [numberOfClicks, setNumberOfClicks] = useState(0);
   const [index, setIndex] = useState(0);
+
  // Click Me Button
-  const clickMEButton = () => {
+  const clickMeButton = () => {
     const randomIndex = Math.floor(Math.random() * listOfColors.length);
     setIndex(randomIndex);
-    updateColors(randomIndex);
+    setNumberOfClicks(numberOfClicks + 1);
   }
 // Click Next Button
   const clickNextButton = () => {
     const nextIndex = (index + 1) % listOfColors.length;
     setIndex(nextIndex);
-    updateColors(nextIndex);
+    setNumberOfClicks(numberOfClicks + 1);
   }
 // Click Previous Button
   const clickPreviousButton = () => {
     const prevIndex = (index - 1 + listOfColors.length) % listOfColors.length;
     setIndex(prevIndex);
-    updateColors(prevIndex);
+    setNumberOfClicks(numberOfClicks + 1)
   }
-// Update Colors
-  const updateColors = (colorIndex) => {
-    const color = listOfColors[colorIndex];
-    document.querySelector('.background-switcher-container').style.backgroundColor = color;
-    document.querySelector('.color-switcher-text-color').style.color = color;
-    document.querySelector('.color-switcher-text-color').textContent = color;
 
-    setCurrentColor(color);
-    setNumberOfClicks(numberOfClicks + 1);
-  }
 // Handle Reset Button
   const handleReset = () => {
-    resetButton(setCurrentColor, setNumberOfClicks, setIndex);
+    setIndex(0);
+    setNumberOfClicks(0);
+    resetButton();
   }
 
 
   return (
-    <div className='background-switcher-container'>
+    <div className='background-switcher-container' style ={{backgroundColor: listOfColors[index]}}>
       <div className='background-switcher-text-container'>
         <div className='background-switcher-display'> 
         <p>Number of Clicks: {numberOfClicks}</p>
         </div>
         <h2 className='color-switcher-text-background'> Background Color: </h2>
-        <h2 className='color-switcher-text-color'> {currentColor} </h2>
+        <h2 className='color-switcher-text-color' style = {{color: listOfColors[index]}}> {listOfColors[index]} </h2>
       </div>
       <div className='color-switcher-reset-container'>
         <button className='color-switcher-button' onClick={handleReset}> RESET </button>
       </div>
       <div className='color-switcher-button-container'>
         <button className='color-switcher-button'onClick={clickPreviousButton}> PREVIOUS </button>
-        <button className='color-switcher-button' onClick={clickMEButton}> CLICK ME </button>
+        <button className='color-switcher-button' onClick={clickMeButton}> CLICK ME </button>
         <button className='color-switcher-button' onClick={clickNextButton}> NEXT </button>
       </div>
     </div>
---
+++ src/assets/components/listOfColors.js
@@ -0,0 +1,3 @@
+
+  // List Of Colors
+ export const listOfColors = ['#AD636C', '#009B72', '#6761A8', '#347E8D', '#798186', '#B4D2BA', '#B57F50', '#8ED081', '#F00000', '#bdc3c7'];
